795b15f50c949b490ad1b533746f4e4da4adfb6d
unknown <triinmirjamtark@gmail.com>
2019-04-01 21:25:38 +0300

    Updates

---
+++ precache-manifest.46f46721c1ae2f9a1104da2d56f8edae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53d0e96f72ae84aaed47",
+    "url": "/static/js/main.eb7f49a8.chunk.js"
+  },
+  {
+    "revision": "4d7734ec9fbba6ea70cd",
+    "url": "/static/js/2.de87e1e9.chunk.js"
+  },
+  {
+    "revision": "53d0e96f72ae84aaed47",
+    "url": "/static/css/main.aa435b37.chunk.css"
+  },
+  {
+    "revision": "7008f4a7fa224f9f0ba87ba629c4beab",
+    "url": "/index.html"
+  }
+];
